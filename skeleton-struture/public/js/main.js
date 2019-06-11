c6b27aa563c775db93f6945bb8fffd69be0b341c
aniketjha1729 <kumaraniket1729@gmail.com>
2019-06-12 05:02:29 +0530

    structure-half-done

--- skeleton-struture/public/js/main.js
+++ skeleton-struture/public/js/main.js
@@ -1,8 +1,15 @@
 function add() {
     var temp = document.getElementById("mySelect").value
+    var arraylist = ["division-style1", "division-style2", "division-style3", "division-style4"];
     for (var i = 0; i < temp; i++) {
-        var para = document.createElement("input");
-        var element = document.getElementById("added");
-        element.appendChild(para);
+        var divison = document.createElement("div");
+        var divclassname=arraylist[i];
+        divison.classList.add(divclassname);
+        for(var j=0;j<4;j++){
+            var inputfield=document.createElement("input");
+            inputfield.classList.add("input-style")
+            divison.appendChild(inputfield);
+        }
+        document.getElementById("added").appendChild(divison);
     }
 }
