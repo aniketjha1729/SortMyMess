181621c118bfa93a4ec52d1e64bceeb8e3443aa8
aniketjha1729 <kumaraniket1729@gmail.com>
2019-06-12 14:33:06 +0530

    form-added

--- skeleton-struture/public/js/main.js
+++ skeleton-struture/public/js/main.js
@@ -1,26 +1,25 @@
 function add() {
-    //var temp = document.getElementById("mySelect").value
-    //var array_input=["Name","Email","Phone"]
+    var temp = document.getElementById("mySelect").value
+    var array_input=["Name","Email","Phone"]
     var form=document.createElement("form");
-    form.classList.add("myform");
+    form.classList.add("form-style");
     form.action="/post";
     form.method="POST";
-    var inputfield1=document.createElement("input");
-    var inputfield2=document.createElement("input");
-    form.appendChild(inputfield1);
-    form.appendChild(inputfield2);
+    var button=document.createElement("button");
+    button.classList.add("btn","btn-success");
+    button.appendChild(document.createTextNode("Submit"));
+    for (var i = 0; i < temp; i++) {
+        var divison = document.createElement("div");
+        divison.classList.add("div-style");
+        for(var j=0;j<3;j++){
+            var inputfield=document.createElement("input");
+            inputfield.classList.add("input-style")
+            inputfield.placeholder=array_input[j];
+            inputfield.name=array_input[j].concat(i.toString());
+            divison.appendChild(inputfield);
+        }
+        form.appendChild(divison);
+    }
+    form.appendChild(button);
     document.getElementById("container").appendChild(form);
-
-    // for (var i = 0; i < temp; i++) {
-    //     var divison = document.createElement("div");
-    //     divison.classList.add("div-style");
-    //     for(var j=0;j<3;j++){
-    //         var inputfield=document.createElement("input");
-    //         inputfield.classList.add("input-style")
-    //         inputfield.placeholder=array_input[j];
-    //         inputfield.name=array_input[j].concat(i.toString());
-    //         divison.appendChild(inputfield);
-    //     }
-    //     document.getElementById("added").appendChild(divison);
-    // }
 }
