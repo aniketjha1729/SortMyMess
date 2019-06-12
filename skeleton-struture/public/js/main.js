38afa36eba336ca49cedfb031536bb4237b1b51a
aniketjha1729 <kumaraniket1729@gmail.com>
2019-06-12 13:51:51 +0530

    bug-found

--- skeleton-struture/public/js/main.js
+++ skeleton-struture/public/js/main.js
@@ -1,15 +1,26 @@
 function add() {
-    var temp = document.getElementById("mySelect").value
-    var arraylist = ["division-style1", "division-style2", "division-style3", "division-style4"];
-    for (var i = 0; i < temp; i++) {
-        var divison = document.createElement("div");
-        var divclassname=arraylist[i];
-        divison.classList.add(divclassname);
-        for(var j=0;j<4;j++){
-            var inputfield=document.createElement("input");
-            inputfield.classList.add("input-style")
-            divison.appendChild(inputfield);
-        }
-        document.getElementById("added").appendChild(divison);
-    }
+    //var temp = document.getElementById("mySelect").value
+    //var array_input=["Name","Email","Phone"]
+    var form=document.createElement("form");
+    form.classList.add("myform");
+    form.action="/post";
+    form.method="POST";
+    var inputfield1=document.createElement("input");
+    var inputfield2=document.createElement("input");
+    form.appendChild(inputfield1);
+    form.appendChild(inputfield2);
+    document.getElementById("container").appendChild(form);
+
+    // for (var i = 0; i < temp; i++) {
+    //     var divison = document.createElement("div");
+    //     divison.classList.add("div-style");
+    //     for(var j=0;j<3;j++){
+    //         var inputfield=document.createElement("input");
+    //         inputfield.classList.add("input-style")
+    //         inputfield.placeholder=array_input[j];
+    //         inputfield.name=array_input[j].concat(i.toString());
+    //         divison.appendChild(inputfield);
+    //     }
+    //     document.getElementById("added").appendChild(divison);
+    // }
 }
