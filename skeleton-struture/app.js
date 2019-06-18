b9bb53eb4c238d0169a6e31d91c4cb6671b6dc4e
aniketjha1729 <kumaraniket1729@gmail.com>
2019-06-18 22:31:44 +0530

    reg-signin-structure-complete

--- skeleton-struture/app.js
+++ skeleton-struture/app.js
@@ -4,16 +4,21 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 const methodOverrid = require('method-override');
+const mongoose=require("mongoose");
 const PORT = process.env.PORT || 1234;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-//###################//
-//Method Overrid
-//###################//
+
 app.use(methodOverrid('_method'));
 app.engine('handlebars', exphbs({ defaultLayout: ''}));
 app.set('view engine', 'handlebars');
+
+
+mongoose.connect("mongodb://localhost/Mess", { useNewUrlParser: true }, () => {
+  console.log("Database Connected");
+});
+
 
 const home = require('./routes/index');
 app.use('/', home);
--- skeleton-struture/routes/index.js
+++ skeleton-struture/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 router.get('/', (req, res) => {
     res.render('index'); 
@@ -8,9 +10,44 @@ router.post("/register",(req,res)=>{
     res.send("Working");
 });
 router.get("/new",(req,res)=>{
-    res.render("test");
-})
+    res.render("test1");
+});
+router.get("/member_reg",(req,res)=>{
+    res.render("memberreg");
+});
+router.get("/member_signin", (req, res) => {
+    res.render("membersigin");
+});
+router.post("/member_reg", (req, res) => {
+    res.send("memberreg");
+});
+router.post("/member_signin", (req, res) => {
+    res.send("membersigin");
+});
+
 router.post("/registermess",(req,res)=>{
-    res.send("Working");
+    const { messid, messname, password} = req.body;
+    User.findOne({messid:messid}).then(user => {
+        if (user) {
+            //errors.push({ msg: 'Email already exits' });
+            res.send("MessId already exists");
+        } else {
+            const newUser = new User({
+                messid,
+                messname,
+                password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    newUser.password = hash;
+                    newUser.save().then(savedUser => {
+                        //req.flash('success_msg', 'Yor are now registered,please login');
+                        res.render("index");
+                    })
+                })
+            })
+        }
+    });
 });
 module.exports = router;
