c8ea26c0a20d9202daf946eeefbf3fd84fc14307
aniketjha1729 <kumaraniket1729@gmail.com>
2019-06-19 09:45:11 +0530

    oneid-oneuser-done

--- skeleton-struture/routes/member.js
+++ skeleton-struture/routes/member.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Member = require("../models/members");
+const User=require("../models/user");
 const bcrypt = require("bcryptjs");
 router.get("/member_reg", (req, res) => {
     res.render("memberreg");
@@ -9,13 +10,17 @@ router.get("/member_signin", (req, res) => {
     res.render("membersigin");
 });
 router.post("/member_reg", (req, res) => {
-            const {name,email,phone,password}=req.body;
-            Member.findOne({email:email}).then(member=>{
-                if(member){
-                    res.send("Member is already Registerd");
+    const messid1=req.body.messid1;
+    User.findOne({messid:messid1}).then(user=>{
+        if(user){
+            const { name, email, phone, password } = req.body;
+            Member.findOne({email:email}).then(user1=>{
+                if(user1){
+                    res.send("Member Already exists")
                 }else{
-                    const newUser=new Member({
-                        name,email,phone,password
+                   // res.send("correct")
+                    const newUser = new Member({
+                        name, email, phone, password
                     });
                     bcrypt.genSalt(10, (err, salt) => {
                         bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -23,11 +28,15 @@ router.post("/member_reg", (req, res) => {
                             newUser.save().then(savedUser => {
                                 //req.flash('success_msg', 'Yor are now registered,please login');
                                 res.send("success");
-                            })
-                        })
-                    })
+                            });
+                        });
+                    });
                 }
             })
+        }else{
+            res.send("No messid found");
+        }
+    })
 });
 
 router.post("/member_signin", (req, res) => {
