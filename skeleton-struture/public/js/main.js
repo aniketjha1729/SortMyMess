d42f03755726a8d81a29714a8829fecb77431197
aniketjha1729 <kumaraniket1729@gmail.com>
2019-06-25 14:39:46 +0530

    data input model created routes working

--- skeleton-struture/public/js/main.js
+++ skeleton-struture/public/js/main.js
@@ -1,28 +1,43 @@
-function add() {
-    var temp = document.getElementById("mySelect").value
-    var input_name=["Name","Email","Phone","Password"];
-    var input_type=["text","email","number","password"];
-    var form=document.createElement("form");
-    form.classList.add("form-style");
-    form.action="/register";
-    form.method="post";
-    var button=document.createElement("button");
-    button.classList.add("btn","btn-success");
-    button.type="Submit";
-    button.appendChild(document.createTextNode("Submit"));
-    for (var i = 0; i < temp; i++) {
-        var divison = document.createElement("div");
-        divison.classList.add("div-style");
-        for(var j=0;j<4;j++){
-            var inputfield=document.createElement("input");
-            inputfield.classList.add("input-style");
-            inputfield.setAttribute("type",input_type[j]);
-            inputfield.placeholder=input_name[j];
-            inputfield.name=input_name[j].concat(i.toString());
-            divison.appendChild(inputfield);
-        }
-        form.appendChild(divison);
+
+// function add() {
+//     var temp = document.getElementById("mySelect").value
+//     var input_name=["Name","Email","Phone","Password"];
+//     var input_type=["text","email","number","password"];
+//     var form=document.createElement("form");
+//     form.classList.add("form-style");
+//     form.action="/register";
+//     form.method="post";
+//     var button=document.createElement("button");
+//     button.classList.add("btn","btn-success");
+//     button.type="Submit";
+//     button.appendChild(document.createTextNode("Submit"));
+//     for (var i = 0; i < temp; i++) {
+//         var divison = document.createElement("div");
+//         divison.classList.add("div-style");
+//         for(var j=0;j<4;j++){
+//             var inputfield=document.createElement("input");
+//             inputfield.classList.add("input-style");
+//             inputfield.setAttribute("type",input_type[j]);
+//             inputfield.placeholder=input_name[j];
+//             inputfield.name=input_name[j].concat(i.toString());
+//             divison.appendChild(inputfield);
+//         }
+//         form.appendChild(divison);
+//     }
+//     form.appendChild(button);
+//     document.getElementById("container").appendChild(form);
+// }
+function editmodal() {
+    var modal = document.getElementById("edit");
+    //console.log(modal);
+    var span = document.getElementsByClassName("close2")[0];
+    modal.style.display = "block";
+    span.onclick = function () {
+        modal.style.display = "none";
     }
-    form.appendChild(button);
-    document.getElementById("container").appendChild(form);
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
--- skeleton-struture/routes/dashboard.js
+++ skeleton-struture/routes/dashboard.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Member = require("../models/members");
+const User = require("../models/user");
 
-router.post("/showmember",(req,res)=>{
-    messid=req.body.messid;
-    email=req.body.email;
-    Member.find({messid1:messid}).then(mem=>{
-        Member.find({email:email}).then(mem1=>{
-            res.render("dashboard", { mem: mem,mem1:mem1});
-        })
-        
-    })
-});
+
 router.get('/logout', (req, res) => {
     req.logOut();
     req.flash("success_msg","Successfully Logout");
@@ -19,11 +11,24 @@ router.get('/logout', (req, res) => {
 });
 router.get('/edit/:id', (req, res) => {
     Member.findOne({ _id: req.params.id }).then(data => {
-            //res.render("admin/posts/edit", {data: data});
             console.log(data);
             res.send("Id Received");
     });
 });
+router.post("/edit/:id",(req,res)=>{
+    const id=req.params.id;
+    console.log(id);
+    res.send("Working");
+})
+router.get("/show",(req,res)=>{
+    
+        res.redirect("/dashboard");  
+    
+    
+})
+router.get("/show:id",(req,res)=>{
+    res.send("working");
+})
 
 
 module.exports = router;
