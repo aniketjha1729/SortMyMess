08386681c041320bd0900588fd2677448c671ead
aniketjha1729 <kumaraniket1729@gmail.com>
2019-07-08 15:04:36 +0530

    data shown in table

--- public/js/main.js
+++ public/js/main.js
@@ -42,20 +42,20 @@ function editmodal() {
     }
 }
 
-function showmodal() {
-  var modal1 = document.getElementById("show");
-  //console.log(modal);
-  var span1 = document.getElementsByClassName("close")[0];
-  modal1.style.display = "block";
-  span1.onclick = function() {
-    modal1.style.display = "none";
-  };
-  window.onclick = function(event) {
-    if (event.target == modal1) {
-      modal1.style.display = "none";
-    }
-  };
-}
+// function showmodal() {
+//   var modal1 = document.getElementById("show");
+//   //console.log(modal);
+//   var span1 = document.getElementsByClassName("close")[0];
+//   modal1.style.display = "block";
+//   span1.onclick = function() {
+//     modal1.style.display = "none";
+//   };
+//   window.onclick = function(event) {
+//     if (event.target == modal1) {
+//       modal1.style.display = "none";
+//     }
+//   };
+// }
 
 function calculatemodal() {
   var modal1 = document.getElementById("calculate");
@@ -70,4 +70,35 @@ function calculatemodal() {
       modal1.style.display = "none";
     }
   };
+}
+
+
+
+function showmodal() {
+  var item_length = document.getElementsByClassName("item").length;
+  console.log(item_length)
+  var divison = document.getElementById("data")
+  for (var i = 0; i < item_length; i++) {
+    var table_name = document.createElement("table");
+    table_name.style.border = "2px solid red"
+    var item_name = document.getElementsByClassName("item")[i];
+    var item_price = document.getElementsByClassName("price")[i];
+    var x = item_name.childNodes[0].nodeValue;
+    var y = item_price.childNodes[0].nodeValue;
+    var temp_array1 = x.split(",");
+    var temp_array2 = y.split(",");
+    for (var j = 0; j < temp_array1.length; j++) {
+      var table_row = document.createElement("tr");
+      var table_data1 = document.createElement("td");
+      var table_data2 = document.createElement("td");
+      table_data1.textContent = temp_array1[j];
+      table_data2.textContent = temp_array2[j];
+      table_row.appendChild(table_data1)
+      table_row.appendChild(table_data2)
+      table_name.appendChild(table_row)
+    }
+    divison.appendChild(table_name)
+    console.log(temp_array1)
+    console.log(temp_array2)
+  }
 }
